c44fdc1866e7ae03eccc040724fa9069d361cf90
Виктор Милов <>
2026-05-06 11:00:02 +0300

    feat(data_logger): реализация метода startSession

--- data_logger/data_logger.cpp
+++ data_logger/data_logger.cpp
@@ -1,1 +1,13 @@
-// data_logger.cpp
+#include "data_logger.h"
+#include <QtCore/QDir>
+
+DataLogger::DataLogger(const QString &baseDir)
+    : m_base_dir(baseDir)
+{
+    QDir().mkpath(baseDir);
+}
+
+DataLogger::~DataLogger()
+{
+    close();
+}
