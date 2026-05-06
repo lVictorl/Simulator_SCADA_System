917183e35c3460c7499289711b5d2ae8e3d92f56
Кирилл Саламатов <>
2026-05-06 18:28:34 +0300

    feat(virtual_mcu): заглушка основного цикла run() со статической телеметрией

--- virtual_mcu/virtual_mcu.cpp
+++ virtual_mcu/virtual_mcu.cpp
@@ -1,1 +1,11 @@
-// virtual_mcu.cpp
+#include "virtual_mcu.h"
+
+VirtualMCU::VirtualMCU(QObject *parent, int slaveId, double dt)
+    : QThread(parent), m_slave_id(slaveId), m_dt(dt)
+{}
+
+VirtualMCU::~VirtualMCU()
+{
+    stopMCU();
+    wait(3000);
+}
