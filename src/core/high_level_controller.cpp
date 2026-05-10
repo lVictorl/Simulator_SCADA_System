5372e38dbc7cfab58c14b99ec40af84a3fb40495
Кирилл Саламатов <>
2026-05-10 16:00:48 +0300

    feat(controller): дополнение HighLevelController методами управления дросселем и инжекции отказов

--- src/core/high_level_controller.cpp
+++ src/core/high_level_controller.cpp
@@ -1,14 +1,9 @@
-#include "core/high_level_controller.h"
+#include "high_level_controller.h"
 #include "modbus_master_adapter.h"
 
 HighLevelController::HighLevelController(ModbusMasterAdapter *master, int defaultSlave)
     : m_master(master), m_default_slave(defaultSlave)
 {}
-
-int HighLevelController::resolve(int slaveId) const
-{
-    return slaveId < 0 ? m_default_slave : slaveId;
-}
 
 void HighLevelController::start(BreakInMode mode, double duration, double target,
                                  const CriticalLimits &limits, double warmup, int slaveId)
@@ -29,9 +24,66 @@ void HighLevelController::start(BreakInMode mode, double duration, double target
     send(cmd, slaveId);
 }
 
+void HighLevelController::stop(int slaveId)
+{
+    MCUCommand cmd; cmd.command_id = QStringLiteral("stop");
+    send(cmd, slaveId);
+}
+
+void HighLevelController::emergencyStop(int slaveId)
+{
+    MCUCommand cmd; cmd.command_id = QStringLiteral("emergency_stop");
+    send(cmd, slaveId);
+}
+
+void HighLevelController::nextStage(int slaveId)
+{
+    MCUCommand cmd; cmd.command_id = QStringLiteral("next_stage");
+    send(cmd, slaveId);
+}
+
+void HighLevelController::resetEmergency(int slaveId)
+{
+    MCUCommand cmd; cmd.command_id = QStringLiteral("reset_emergency");
+    send(cmd, slaveId);
+}
+
+void HighLevelController::setThrottleManual(double value, int slaveId)
+{
+    MCUCommand cmd;
+    cmd.command_id    = QStringLiteral("set_throttle");
+    cmd.manual_throttle = true;
+    cmd.throttle_value  = value;
+    send(cmd, slaveId);
+}
+
+void HighLevelController::setThrottleAuto(int slaveId)
+{
+    MCUCommand cmd;
+    cmd.command_id    = QStringLiteral("set_throttle");
+    cmd.manual_throttle = false;
+    send(cmd, slaveId);
+}
+
+void HighLevelController::injectFault(int sensorIdx, SensorFaultType type, int slaveId)
+{
+    MCUCommand cmd;
+    cmd.command_id     = QStringLiteral("inject_fault");
+    cmd.fault_sensor_idx = sensorIdx;
+    cmd.fault_type     = type;
+    send(cmd, slaveId);
+}
+
+void HighLevelController::clearFault(int sensorIdx, int slaveId)
+{
+    MCUCommand cmd;
+    cmd.command_id     = QStringLiteral("clear_fault");
+    cmd.fault_sensor_idx = sensorIdx;
+    send(cmd, slaveId);
+}
+
 void HighLevelController::send(MCUCommand cmd, int slaveId)
 {
     cmd.slave_id = resolve(slaveId);
     m_master->sendCommand(cmd);
 }
-// TODO: Реализовать остальные методы контроллера (stop, emergencyStop, nextStage, resetEmergency, throttle, injectFault, clearFault)
